a8d02c5b0eccc68db45bd89fa368b6fdb80e1099
Aurora <126132191+auroraaqilla@users.noreply.github.com>
2023-06-17 15:23:02 +0700

    Add files via upload

---
+++ javascript/javascript/script/HelloWorld.js
@@ -0,0 +1,27 @@
+document.writeln("Hello World");
+document.writeln("<br/>");
+document.writeln("NAMA : " + "  Aurora ");
+document.write("<br/>");
+
+let fullName = "Alivia Aqilla Fadia Aurora";
+let firstName = "Alivia";
+let middleName = "Aqilla Fadia";
+let lastName = "Aurora";
+
+document.writeln("Nama Lengakap : " + fullName);
+document.write("<br/>");
+document.writeln("Nama Pertama : " + firstName);
+document.write("<br/>");
+document.writeln("Nama Tengah : " + middleName);
+document.write("<br/>");
+document.writeln("Nama Akhir : " + lastName);
+document.writeln("<br/>");
+
+let result = 1 + 2;
+document.writeln;
+
+let counter = 1;
+for (; counter <= 10; ) {
+  document.writeln("<p>Nomor" + counter + "</p>");
+  counter++;
+}
